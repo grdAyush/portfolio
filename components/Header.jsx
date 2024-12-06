cd7cf170501bc6de1161930e6d0d7517573dd6f8
grdAyush <ayush123@duck.com>
2024-12-06 16:37:20 +0530

    Nav Bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo*/}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
@@ -14,12 +14,15 @@ const Header = () => {
         </Link>
 
         {/* desktop Nav */}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire Me</Button>
           </Link>
         </div>
+
+        {/* mobile Nav */}
+        <div className="xl:hidden"> Mobile No. </div>
       </div>
     </header>
   );
